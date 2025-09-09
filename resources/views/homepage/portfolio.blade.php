7ae364928787e3336546f71a3594801828b06161
rizkitaufiqgit config --global user.email <rizkitaufiq97@gmail.com>
2025-09-09 16:53:38 +0700

    feat: add content

--- resources/views/homepage/portfolio.blade.php
+++ resources/views/homepage/portfolio.blade.php
@@ -1,5 +1,6 @@
 <!-- start portfolio items -->
-<section class="live-tranding-area tranding_bg-shape rts-section-gapBottom rt_bg-secondary" id="portfolio">
+<section style="position: relative" class="live-tranding-area tranding_bg-shape rts-section-gapBottom rt_bg-secondary"
+    id="portfolio">
     <div class="container">
         <div class="row">
             <div class="col-12">
@@ -21,25 +22,21 @@
                                     <a href="#portfolio"><img
                                             style="width:100%; height:200px; object-fit:cover; border-radius:8px;"
                                             src="assets/images/product/game/working-at-heights-training.webp"
-                                            loading="lazy" alt="Nft_product"></a>
+                                            loading="lazy" alt="product"></a>
                                 </div>
-                                <div class="product-discription" style="flex-grow:1; margin:1rem 0;">
+                                <div class="product-discription" style="flex-grow:1;">
                                     <div class="product-left">
-                                        <h5 class="title" style="margin:0 0 0.5rem 0;">
+                                        <h5 class="title">
                                             Working at Heights
                                             Training
                                         </h5>
-                                        <p class="price"
-                                            style="margin:0; font-size:14px; line-height:1.4; text-align:justify;">This
+                                        <p class="price" style="margin:0; font-size:14px; text-align:justify;">This
                                             Virtual Reality simulation trains users on safety checks
                                             for scaffolding and
                                             body harness use, including tagging verification and proper HOK attachment.
                                             Successfully reaching the top completes the mission.
                                         </p>
                                     </div>
-                                </div>
-                                <div class="product-right" style="display:flex; justify-content: end;">
-                                    <a href="" class="rts-btn btn-secondary radious-5">Play</a>
                                 </div>
                             </div>
                             <!-- single slider End-->
@@ -53,7 +50,7 @@
                                     <a href="#portfolio"><img
                                             style="width:100%; height:200px; object-fit:cover; border-radius:8px;"
                                             src="assets/images/product/game/confined-space-training.webp" loading="lazy"
-                                            alt="Nft_product"></a>
+                                            alt="product"></a>
                                 </div>
                                 <div class="product-discription" style="flex-grow:1; margin:1rem 0;">
                                     <div class="product-left" style="margin-bottom: 2rem">
@@ -66,13 +63,9 @@
                                             procedures in confined
                                             spaces, including checklist verification, oxygen tank inspection, blower
                                             activation, and gas detection. Interactive controls guide users through each
-
                                             V step, ensuring proper understanding before starting tasks. irtual Reality
                                         </p>
                                     </div>
-                                </div>
-                                <div class="product-right" style="display:flex; justify-content: end;">
-                                    <a href="" class="rts-btn btn-secondary radious-5">Play</a>
                                 </div>
                             </div>
                             <!-- single slider End-->
@@ -86,7 +79,7 @@
                                     <a href="#portfolio"><img
                                             style="width:100%; height:200px; object-fit:cover; border-radius:8px;"
                                             src="assets/images/product/game/fire-extingsher-simulation.webp"
-                                            loading="lazy" alt="Nft_product"></a>
+                                            loading="lazy" alt="product"></a>
                                 </div>
                                 <div class="product-discription" style="flex-grow:1; margin:1rem 0;">
                                     <div class="product-left" style="margin-bottom: 2rem">
@@ -104,8 +97,35 @@
                                         </p>
                                     </div>
                                 </div>
-                                <div class="product-right" style="display:flex; justify-content: end;">
-                                    <a href="" class="rts-btn btn-secondary radious-5">Play</a>
+                            </div>
+                            <!-- single slider End-->
+                        </div>
+
+                        <div class="swiper-slide" style="height:100%; display:flex;">
+                            <!-- single slider -->
+                            <div class="trending-items_wrapper"
+                                style="display:flex; flex-direction:column; justify-content:space-between; height:100%; min-height:450px; padding:1rem; box-sizing:border-box;">
+                                <div class="thumbnail">
+                                    <a href="#portfolio"><img
+                                            style="width:100%; height:200px; object-fit:cover; border-radius:8px;"
+                                            src="assets/images/product/game/hot-work-supervision.webp" loading="lazy"
+                                            alt="product"></a>
+                                </div>
+                                <div class="product-discription" style="flex-grow:1; margin:1rem 0;">
+                                    <div class="product-left" style="margin-bottom: 2rem">
+                                        <h5 class="title" style="margin:0 0 0.5rem 0;">
+                                            Hot Work
+                                            Supervision
+                                        </h5>
+                                        <p class="price"
+                                            style="margin:0; font-size:14px; line-height:1.4;text-align:justify;">This
+                                            This Virtual Reality simulation guides users through safety checks for hot
+                                            work,
+                                            including PPE inspection, gas detection, and fire hazard removal. Completing
+                                            all checklist items allows users to issue welding permits and finish the
+                                            mission successfully.
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                             <!-- single slider End-->
@@ -113,9 +133,24 @@
 
                     </div>
                     <div class="swiper-pagination"></div>
+
                 </div>
             </div>
         </div>
     </div>
+
+    <svg width="5%" style="position: absolute; top: 50%; left: 6%; transform: translate(-50%, -50%); z-index:2;"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 256 512"><!--!Font Awesome Free v6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+        <path fill="rgba(255,255,255,0.5)"
+            d="M9.4 278.6c-12.5-12.5-12.5-32.8 0-45.3l128-128c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6l0 256c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-128-128z" />
+    </svg>
+
+    <svg width="5%" style="position: absolute; top: 50%; right: 2%; transform: translate(-50%, -50%); z-index:2;"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 256 512"><!--!Font Awesome Free v6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+        <path fill="rgba(255,255,255,0.5)"
+            d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z" />
+    </svg>
 </section>
 <!-- end portfolio items -->
