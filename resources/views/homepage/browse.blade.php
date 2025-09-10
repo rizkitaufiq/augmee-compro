d00e4490d4536b46a50de37551f15fb97ce482b1
rizkitaufiqgit config --global user.email <rizkitaufiq97@gmail.com>
2025-09-10 14:08:05 +0700

    feat: add browse section

---
+++ resources/views/homepage/browse.blade.php
@@ -0,0 +1,32 @@
+<!-- start browse area -->
+<section class="rts-brad-crumb-area breadcrumb-image bg_image">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="breadcrumb-inner ptb--130 pt_sm--80 pb_sm--80" style="gap: 2rem">
+                    <div style="display: flex;flex-direction:column;">
+                        <p>To become the leading company in AR, VR, Game Development, and 3D Modeling in Indonesia,
+                            while showcasing our creations to a global audience.</p>
+                        <div class="breadcrumb-list" style="width: 10vh">
+                            <div>
+                                <h5>Get to know us better — click here <br> to see our company profile</h5>
+                                <a href="https://drive.google.com/file/d/1uyeBdJMecqdpqjIxJbeoEcdqAbWJro6C/view?usp=sharing"
+                                    class="rts-btn btn-primary btn-lg radious-5" data-sal-delay="600"
+                                    data-sal-duration="1000" data-sal="slide-up">Visit</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div
+                        style="gap:1rem;display: flex; flex-direction:column; justify-content:start; align-items:center;">
+                        <img src="assets/images/browse/qrcode-augmee.png" width="40%" loading="lazy" alt="profile">
+                        <p>Augmee Company Profile</p>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End browse area -->
